06564cd155e58aa7022a25db4b24f928a14b4b1b
Maxim345525 <maximmaksimchyk@gmail.com>
2024-02-19 20:16:52 +0200

    add code

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,75 @@
-s
+#include <iostream>
+#include <string>
+using namespace std;
+class Weapon {
+public:
+    virtual ~Weapon() {}
+    virtual void attack() const = 0;
+    virtual string getType() const = 0;
+};
+class Sword : public Weapon {
+public:
+    void attack() const override {
+        cout << "I attack with a sword!" << endl;
+    }
+
+    string getType() const override {
+        return "Sword";
+    }
+};
+
+
+class Bow : public Weapon {
+public:
+    void attack() const override {
+        cout << "I shoot with a bow!" << endl;
+    }
+
+    string getType() const override {
+        return "Bow";
+    }
+};
+
+class Dagger : public Weapon {
+public:
+    void attack() const override {
+        cout << "I hit with a dagger!" << endl;
+    }
+
+    string getType() const override {
+        return "Dagger";
+    }
+};
+
+
+class WeaponFactory {
+public:
+    static Weapon* createWeapon(const std::string& type) {
+        if (type == "Sword") {
+            return new Sword();
+        }
+        else if (type == "Bow") {
+            return new Bow();
+        }
+        else if (type == "Dagger") {
+            return new Dagger();
+        }
+        else {
+            return nullptr;
+        }
+    }
+};
+
+int main() {
+    Weapon* playerWeapon = WeaponFactory::createWeapon("Μεχ");
+    if (playerWeapon) {
+        cout << "player choose " << playerWeapon->getType() << endl;
+        playerWeapon->attack();
+        delete playerWeapon;
+    }
+    else {
+        cout << "Unknown weapon type!" << endl;
+    }
+
+    return 0;
+}
